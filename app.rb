a392c51bd4e03893da3822309b3cd26b90e69a21
Stealth-git <rebill2me@gmail.com>
2021-03-25 13:43:07 +0300

    Display msg: Your cart is empty

--- app.rb
+++ app.rb
@@ -37,9 +37,16 @@ get '/index' do
 end
 
 post '/cart' do
+	# получаем список параметров и разбираем их
 	@orders_input = params[:orders_input]
 	@items = parse_orders_input @orders_input
 
+	# выводим сообщение о том, что корзина пуста
+	if @items.length == 0
+		return erb :cart_is_empty
+	end
+
+	# выводим список продуктов в корзине
 	@items.each do |item|
 		# id, cnt
 		item[0] = Product.find(item[0])
